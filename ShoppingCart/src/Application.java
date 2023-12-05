83769b3ac99ba8199a93cc762d9eb81b3f0086cb
Juanjoarci <152829836+Juanjoarci@users.noreply.github.com>
2023-12-05 16:22:47 -0500

    Update Application.java

--- ShoppingCart/src/Application.java
+++ ShoppingCart/src/Application.java
@@ -1,3 +1,8 @@
+/**
+ * Shopping Cart Application
+ * Main class that initializes the login view and the controller
+ * Also displays the login view.
+ */
 public class Application {
     public static void main(String[] args) {
         LoginView view = new LoginView();
