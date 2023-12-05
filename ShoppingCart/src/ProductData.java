4d73e4bad569f48c3aab65b9d45d58e149a822f4
Juanjoarci <152829836+Juanjoarci@users.noreply.github.com>
2023-12-05 18:03:58 -0500

    Update ProductData.java

--- ShoppingCart/src/ProductData.java
+++ ShoppingCart/src/ProductData.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class that provides access to product data for the application.
+ */
 public class ProductData {
     private static final List<Product> products = new ArrayList<>();
-
     static {
-        // Initialize hardcoded products
         products.add(new Product("Water Bottle", 5.00, 10.00, "This is a 16oz water bottle", 10));
         products.add(new Product("Teddy Bear", 10.00, 70.00, "This is a light brown Teddy Bear", 5));
         products.add(new Product("Sunglasses", 5.00, 20.00, "This is a simple pair of black sunglasses", 20));
@@ -13,14 +14,17 @@ public class ProductData {
         products.add(new Product("Monitor", 200.00, 1000.00, "This is a simple 27 inch monitor", 3));
     }
 
+    /**
+     * Add a new product to the product list.
+     * Get a list of available products from the product list.
+     * Update the quantity of a single product from the product list.
+     */
     public static void addProduct(Product product) {
         products.add(product);
     }
-
     public static List<Product> getProducts() {
-        return new ArrayList<>(products); // Return a copy to avoid modification
-    }
-
+        return new ArrayList<>(products);
+    }    
     public static void updateProductQuantity(Product product, int quantityChange) {
         for (Product p : products) {
             if (p.equals(product)) {
@@ -35,6 +39,11 @@ public class ProductData {
     private static int totalSales = 0;
     private static double totalProfit = 0.0;
 
+    /**
+     * A constructor that records a sale and updates the total revenue, total sales, and total profit.
+     * @param product  is the product being sold.
+     * @param quantity is the quantity sold.
+     */
     public static void recordSale(Product product, int quantity) {
         double revenueFromSale = product.getPrice() * quantity;
         double costOfSale = product.getCost() * quantity;
@@ -42,17 +51,13 @@ public class ProductData {
         totalProfit += (revenueFromSale - costOfSale);
         totalSales += quantity;
     }
-
     public static double getTotalRevenue() {
         return totalRevenue;
     }
-
     public static int getTotalSales() {
         return totalSales;
     }
-
     public static double getTotalProfit() {
         return totalProfit;
     }
-
 }
