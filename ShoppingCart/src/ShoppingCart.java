76237f9e85e483896d45e11a8955ecc9fd783ce0
Juanjoarci <152829836+Juanjoarci@users.noreply.github.com>
2023-12-06 13:42:25 -0500

    Update ShoppingCart.java

--- ShoppingCart/src/ShoppingCart.java
+++ ShoppingCart/src/ShoppingCart.java
@@ -2,13 +2,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * ShoppingCart class that represents a shopping cart that holds products and its quantities.
+ */
 public class ShoppingCart {
     private Map<Product, Integer> products;
-
+    
     public ShoppingCart() {
         products = new HashMap<>();
     }
 
+    /**
+     * Adds a product to the shopping cart.
+     * @param product  is the product to be added. Product can't be null
+     * @return True    if the product was added successfully, false otherwise.
+     * If the product is added successfully, the quantity in the cart is increased by 1.
+     */
     public boolean addProduct(Product product) {
         int currentQuantity = products.getOrDefault(product, 0);
         if (currentQuantity < product.getQuantity()) {
@@ -18,6 +27,10 @@ public class ShoppingCart {
         return false;
     }
 
+    /**
+     * Removes a product from the shopping cart.
+     * @param product is the product to be removed. can't be null.
+     */
     public void removeProduct(Product product) {
         int currentQuantity = products.getOrDefault(product, 0);
         if (currentQuantity > 1) {
@@ -27,10 +40,18 @@ public class ShoppingCart {
         }
     }
 
+    /**
+     * Gets the set of products and quantities in the shopping cart.
+     * @return the set of products and quantities.
+     */
     public Set<Map.Entry<Product, Integer>> getProducts() {
         return products.entrySet();
     }
 
+    /**
+     * Calculation of the total cost of all the products in the shopping cart.
+     * @return the total cost of all the products in the shopping cart.
+     */
     public double getTotal() {
         double total = 0.0;
         for (Map.Entry<Product, Integer> entry : products.entrySet()) {
@@ -39,6 +60,7 @@ public class ShoppingCart {
         return total;
     }
 
+    // Clears the shopping cart by removing all the products.
     public void clearCart() {
         products.clear();
     }
